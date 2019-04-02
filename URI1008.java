732e2543900fccc2c36931be10aeedadd96587c4
Bruna Stall <bruna.stall14@gmail.com>
2019-04-01 22:44:21 -0300

    URI Online Judge

---
+++ URI1008.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner ler = new Scanner(System.in);
+
+        int nFunc, nHoras;
+        float hTrabalhada;
+
+        nFunc = ler.nextInt();
+        nHoras = ler.nextInt();
+        hTrabalhada = ler.nextFloat();
+
+        System.out.println("NUMBER = " + nFunc);
+        System.out.printf("SALARY = U$ %.2f\n", (hTrabalhada*nHoras));
+    }
+}
